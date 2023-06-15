324db69fecb221603363d141887b031888dfed77
Theophile-Wemaere <theophile.wemaere@gmail.com>
2023-06-15 23:08:26 +0200

    added insecure handling

--- embedded/uploader.py
+++ embedded/uploader.py
@@ -7,7 +7,7 @@ import os
 
 load_dotenv()
 
-URL = "http://localhost:5000/uploads"
+URL = f"https://{os.getenv('FLASK_HOST')}:5000/uploads"
 id_device = "BFD-0LS-X0Y"
 
 headers = {
@@ -22,8 +22,7 @@ def upload_data(sensor,value):
         'id_device': id_device
     }
     try:
-        r = requests.post(URL,data=json.dumps(body),headers=headers)
-    except requests.exceptions.ConnectionError:
-        pass
-    #print(r,r.text)
-
+        r = requests.post(URL,data=json.dumps(body),headers=headers, verify=False)
+    except requests.exceptions.ConnectionError as e:
+        print(e)
+    
